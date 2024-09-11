450b7f289f0023e6b7444b232e270b5347565823
ToanG1 <Toandinh01675@gmail.com>
2024-09-11 23:14:29 +0700

    login api

--- user/user.controller.ts
+++ user/user.controller.ts
@@ -1,26 +1,57 @@
-import { api } from "encore.dev/api";
-import { createUser } from "./user.service";
+import { api, APIError, ErrCode } from "encore.dev/api";
+import { createUser, findUser } from "./user.service";
 import { send } from "../mail/mail.controller";
+import { createRefreshToken, createToken } from "./jwt.service";
+import { saveRefreshToken } from "./token.service";
 
 const signup = api(
   { expose: true, method: "POST", path: "/auth/signup" },
   async (request: ICreateUserRequest): Promise<IResponse> => {
-    const pwd = await createUser(request.email, request.name);
+    try {
+      const pwd = await createUser(request.email, request.name);
 
-    send({
-      to: request.email,
-      subject: "Your temporary password",
-      text: `Your password is ${pwd}`,
-    });
+      send({
+        to: request.email,
+        subject: "Your temporary password",
+        text: `Your password is ${pwd}`,
+      });
+    } catch (_) {
+    } finally {
+      return {
+        code: 200,
+        message: "Your account has been created!",
+      };
+    }
+  }
+);
+
+const login = api(
+  { expose: true, method: "POST", path: "/auth/login" },
+  async (request: ILoginRequest): Promise<ILoginResponse> => {
+    if (!request.email || !request.password) {
+      throw new APIError(
+        ErrCode.InvalidArgument,
+        "Email and password are required"
+      );
+    }
+
+    const { userId, name } = await findUser(request.email, request.password);
+    if (!name) {
+      throw new APIError(ErrCode.InvalidArgument, "Invalid email or password");
+    }
+
+    const refreshToken = createRefreshToken(name);
+    await saveRefreshToken(userId, refreshToken);
 
     return {
-      code: 200,
-      message: "Your account has been created!",
+      token: createToken(name),
+      refreshToken,
+      name,
     };
   }
 );
 
-export { signup };
+export { signup, login };
 
 interface IResponse {
   code: number;
@@ -29,5 +60,16 @@ interface IResponse {
 
 interface ICreateUserRequest {
   email: string;
+  name: string;
+}
+
+interface ILoginRequest {
+  email: string;
+  password: string;
+}
+
+interface ILoginResponse {
+  token: string;
+  refreshToken: string;
   name: string;
 }
--- user/user.service.ts
+++ user/user.service.ts
@@ -1,10 +1,20 @@
 import { SQLDatabase } from "encore.dev/storage/sqldb";
 import { randomInt } from "node:crypto";
+import { APIError, ErrCode } from "encore.dev/api";
 
 const database = new SQLDatabase("user", { migrations: "./migrations" });
 
 const createUser = async (email: string, name: string): Promise<number> => {
   const randomPwd = randomInt(100000, 999999);
+
+  const result = await database.queryRow`
+        SELECT EXISTS (SELECT * FROM USERS WHERE EMAIL = ${email})
+    `;
+
+  if (result?.exists || false) {
+    throw new APIError(ErrCode.AlreadyExists, "Email already exist");
+  }
+
   await database.exec`
         INSERT INTO USERS (EMAIL, NAME, PASSWORD) 
         VALUES (${email}, ${name}, ${randomPwd})
@@ -13,4 +23,14 @@ const createUser = async (email: string, name: string): Promise<number> => {
   return randomPwd;
 };
 
-export { createUser };
+const findUser = async (email: string, password: string) => {
+  const user = await database.queryRow`
+        SELECT ID, NAME FROM USERS WHERE EMAIL = ${email} AND PASSWORD = ${password}
+    `;
+  return {
+    userId: user?.id ?? undefined,
+    name: user?.name ?? undefined,
+  };
+};
+
+export { createUser, findUser };
